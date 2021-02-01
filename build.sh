68245bbf3afd0aba05b5976895826ba4f8e7d1c5
Junior Santos <claudio.dossantosjunior@yahoo.com.br>
2021-02-01 17:10:31 -0300

    Update ubuntu version, and ftp_version

--- build.sh
+++ build.sh
@@ -4,8 +4,9 @@ del_version=$(cat delete_version.txt)
 
 if [ $github_version != $ftp_version ]
 then
+    cd $GOPATH
     mkdir -p $GOPATH/src/k8s.io
-    cd $GOPATH/src/k8s.io
+    cd src/k8s.io
     wget https://github.com/kubernetes/kubernetes/archive/v$github_version.zip
     unzip v$github_version.zip
     mv kubernetes-$github_version/ kubernetes
@@ -20,7 +21,7 @@ then
     then
         lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; put -O /ppc64el/kubeadm/latest kubeadm-$github_version"
         lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; rm /ppc64el/kubeadm/latest/kubeadm-$ftp_version"
+        lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; put -O /ppc64el/kubeadm kubeadm-$github_version"
+        #lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; rm /ppc64el/kubeadm/kubeadm-$del_version"
     fi
-    lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; put -O /ppc64el/kubeadm kubeadm-$github_version"
-    #lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; rm /ppc64el/kubeadm/kubeadm-$del_version"
 fi
